e60a0c717a9477ed2879818ebdc94a12d7a66ad8
Taylor Liang <bu213200@gmail.com>
2017-06-27 17:29:30 +0800

    Update

--- run.py
+++ run.py
@@ -1,14 +1,39 @@
 #!/usr/bin/python
 # filename: run.py
 import re
+import sys
+import datetime
+import time
 from crawler import Crawler, CrawlerCache
 
-if __name__ == "__main__": 
+
+def GetTimeStr(time):
+    return str(now.year % 100) + (('0' + str(now.month)) if now.month < 10 else str(now.month)) + str(now.day)
+
+
+if __name__ == "__main__":
     # Using SQLite as a cache to avoid pulling twice
-    crawler = Crawler(CrawlerCache('crawler.db'))
+    crawler = Crawler(CrawlerCache('crawler.db'), 1)
     root_re = re.compile('^/$').match
-    crawler.crawl('http://techcrunch.com/', no_cache=root_re)
-    crawler.crawl('http://www.engadget.com/', no_cache=root_re)
-    crawler.crawl('http://gizmodo.com/', no_cache=root_re)
-    crawler.crawl('http://www.zdnet.com/', no_cache=root_re)
-    crawler.crawl('http://www.wired.com/', no_cache=root_re)
+
+    now = datetime.datetime.now()
+    nowString = GetTimeStr(now)
+
+    with open("LowPriceOutput.txt", "a") as myfile:
+        myfile.write(nowString + " (CheckDate): \n")
+
+    for i in range(0, 180):
+        nowString = GetTimeStr(now)
+        print('Start retrieving ' + nowString)
+        crawler.crawl('http://www.flycua.com/flight2014/nay-hny-' + nowString + '_CNY.html', no_cache=root_re)
+        print(crawler.content['www.flycua.com']['/flight2014/nay-hny-' + nowString + '_CNY.html'])
+        print('End retrieving ' + nowString)
+
+        if True:
+            with open("LowPriceOutput.txt", "a") as myfile:
+                myfile.write("nay-hny-" + nowString + ", Price: " + str(499) + "\n")
+
+        now += datetime.timedelta(days=1)
+        time.sleep(10)
+
+    sys.exit(0)
